0c86d0478056051d5bd7b9e753cb99d67e140c91
irmaodejesus <emmanuel.peixoto@sga.pucminas.br>
2024-10-29 22:15:26 -0300

    "melhorias"

--- etapa3-pucminas/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ etapa3-pucminas/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ab0f2b867e0b6713934b1d1d0540c18f55b0cbb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60a7bd8a34ddffc0bf7ba7e08cba468e5a928845")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
