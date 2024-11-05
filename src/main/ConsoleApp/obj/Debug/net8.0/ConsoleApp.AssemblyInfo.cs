ddf023f4d1a5d6390d34ae78b04e93cd622faa24
irmaodejesus <emmanuel.peixoto@sga.pucminas.br>
2024-11-04 23:24:17 -0300

    "Oraganizacao do coigo"

--- src/main/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ src/main/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6deef41c838cfe2500953d5933792073920f76f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e8b26976a45220ebe6be1db842eb6908f631011")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
