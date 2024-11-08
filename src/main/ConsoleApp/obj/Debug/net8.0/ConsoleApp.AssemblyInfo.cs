1afc5a4ae58e459b3175042a2fecba78d949965b
irmaodejesus <emmanuel.peixoto@sga.pucminas.br>
2024-11-08 17:23:35 -0300

    ""

--- src/main/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ src/main/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f88517e4cc2a0588602305809f943ac31de34df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+759a56f914961da6ac4e274ef6166a70d9eb78f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
